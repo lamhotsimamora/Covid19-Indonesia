75ae08aa33ce7c2b3faf626ca87b1721d4992495
Lamhot Simamora <lamhotsimamora36@gmail.com>
2020-03-29 20:44:39 +0700

    +

--- js/server.js
+++ js/server.js
@@ -1,4 +1,4 @@
 const API_Covid                   = 'https://corona.lmao.ninja/countries/';
 const API_Covid_All_Country_Count = 'https://corona.lmao.ninja/all';
-const API_Covid_All_Country       = 'https://pomber.github.io/covid19/timeseries.json';
+//const API_Covid_All_Country       = 'https://pomber.github.io/covid19/timeseries.json';
 const API_DATE                    = 'https://pdrtechnology.net/date.php';
