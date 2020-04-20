d4490a4c3ec4adac658036b2fa60ee9bf08b757c
Lamhot Simamora <lamhotsimamora36@gmail.com>
2020-04-20 22:56:38 +0700

    + change endpoint API

--- js/server.js
+++ js/server.js
@@ -1,7 +1,8 @@
-const API_Covid                   = 'https://corona.lmao.ninja/countries/';
-const API_Covid_All_Country_Count = 'https://corona.lmao.ninja/all';
+const API_Covid                   = 'https://corona.lmao.ninja/v2/countries/';
+const API_Covid_All_Country_Count = 'https://corona.lmao.ninja/v2/all';
 const API_Covid_All_Provinsi      = 'https://api.kawalcorona.com/indonesia/provinsi/';
 const API_DATE                    = 'https://pdrtechnology.net/date.php';
+
 
 let $loading = new Vue({
 	el : '#loading',
